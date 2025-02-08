71d2969b0287defbfa482c3a5b349a7c6990b0e5
GuillermoK1 <guillermojkr@gmail.com>
2025-02-08 11:19:15 +0000

    Added the OAuth function and it works ok.

---
+++ pages/logged-in.vue
@@ -0,0 +1,5 @@
+<template>
+  <UContainer>
+    <h1>You're logged in!</h1>
+  </UContainer>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p class="p-2">
+      · No almacenamos contraseñas. <br>
+      · Por seguridad, es necesario iniciar sesión con Google para continuar. <br>
+      · Iniciar sesión no implica registrarse. <br>
+    </p>
+    <UButton
+      class="shadow-xl shadow-black z-10 w-64 m-3"
+      to="/api/auth/google"
+      icon="i-simple-icons-google"
+      label="Continuar con Google"
+      size="xl"
+      external
+      color="blue"
+    />
+  </div>
+</template>
